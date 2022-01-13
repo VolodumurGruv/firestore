3bae6c7424f12cda5854140a16a3ff7e5bb62093
VolodumurGruv <gru.v@i.ua>
2022-01-13 23:48:33 +0200

    made basic template for upload an image

--- src/app/components/upload-images/upload-images.component.ts
+++ src/app/components/upload-images/upload-images.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-upload-images',
   templateUrl: './upload-images.component.html',
-  styleUrls: ['./upload-images.component.scss']
+  styleUrls: ['./upload-images.component.scss'],
 })
 export class UploadImagesComponent implements OnInit {
+  public imageName!: string;
+  public path!: string;
+  public imageForm = this.fb.group({ image: [''] });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  imageSelected(event: any) {
+    const file: File = event.target.files[0];
+    const formData = new FormData();
+
+    formData.append('beefamily', file);
+    this.imageName = file.name;
+    this.imageForm.setValue(formData);
   }
 
+  onSubmit() {
+    console.log(this.imageForm.value);
+    this.imageForm.reset();
+    this.imageName = '';
+  }
 }
